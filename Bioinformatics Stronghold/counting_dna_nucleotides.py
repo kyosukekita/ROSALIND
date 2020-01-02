8583fbf1dfb3503f9cdc5ae24adcc55cab5f0fde
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2020-01-02 16:25:33 +0900

    Create counting_dna_nucleotides.py

---
+++ Bioinformatics Stronghold/counting_dna_nucleotides.py
@@ -0,0 +1,9 @@
+#counting_dna_nucleotides.py
+
+nucleotides=""
+
+str_line=str(nucleotides)
+print(str_line.count('A'))
+print(str_line.count('C'))
+print(str_line.count('G'))
+print(str_line.count('T'))

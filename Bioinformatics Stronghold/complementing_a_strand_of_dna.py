8b7fbdb3d8809132524e811cf6f79286d7346961
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2020-01-02 16:38:47 +0900

    Create complementing_a_strand_of_dna.py

---
+++ Bioinformatics Stronghold/complementing_a_strand_of_dna.py
@@ -0,0 +1,9 @@
+file=open('rosalind_revc.txt','r')
+for line in file:
+    str_line=str(line)
+
+def reverse_complement(sequence):
+    temp=sequence.replace("A","x").replace("T","A").replace("x","T")
+    return (temp.replace("G","x").replace("C","x").replace("x","C")[::-1])
+
+reverse_complement(str_line)

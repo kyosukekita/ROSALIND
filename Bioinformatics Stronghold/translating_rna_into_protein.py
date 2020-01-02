e966ae40da51352d41190cd777a67831a1f4524d
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2020-01-02 20:08:16 +0900

    Create translating_rna_into_protein.py

---
+++ Bioinformatics Stronghold/translating_rna_into_protein.py
@@ -0,0 +1,86 @@
+sequence="""AGCAGCACCAGC"""
+print(len(sequence))
+codon=[sequence[i:i+3] for i in range(0,len(sequence),3)]
+
+print(codon)
+def translate(x):
+    if x=="UUU"or x=="UUC":
+        return "F"
+    elif x=="UUA"or x=="UUG"or x=="CUU"or x=="CUC"or x=="CUA"or x=="CUG":
+        return "L"
+    elif x=="AUU"or x=="AUC" or x=="AUA":
+        return "I"
+    elif x=="AUG":
+        return "M"
+    elif x=="GUU"or x=="GUC"or x=="GUA"or x=="GUG":
+        return "V"
+    elif x=="UCU"or x=="UCC"or x=="UCA"or x=="UCG":
+        return "S"
+    elif x=="CCU"or x=="CCC"or x=="CCA"or x=="CCG":
+        return "P"
+    elif x=="ACU"or x=="ACC"or x=="ACA"or x=="ACG":
+        return "T"
+    elif x=="GCU"or x=="GCG"or x=="GCA"or x=="GCC":
+        return "A"
+    elif x=="UAU"or x=="UAC":
+        return "Y"
+    elif x=="CAU"or x=="CAC":
+        return "H"
+    elif x=="CAA"or x=="CAG":
+        return "Q"
+    elif x=="AAU"or x=="AAC":
+        return "N"
+    elif x=="AAA"or x=="AAG":
+        return "K"
+    elif x=="GAU"or x=="GAC":
+        return "D"
+    elif x=="GAA"or x=="GAG":
+        return "E"
+    elif x=="UGU"or x=="UGC":
+        return "C"
+    elif x=="UGG":
+        return "W"
+    elif x=="CGU"or x=="CGA"or x=="CGC"or x=="CGG"or x=="AGA"or x=="AGG":
+        return "R"
+    elif x=="AGU"or x=="AGC":
+        return "S"
+    elif x=="GGU"or x=="GGC"or x=="GGA"or x=="GGG":
+        return "G"
+    elif x=="UGA" or x=="UAA" or x=="UAG":
+        return ""
+    else:
+        return"-"
+
+print("".join([translate(x) for x in codon]))
+
+
+
+#another solution
+
+string = """UUU F      CUU L      AUU I      GUU V
+UUC F      CUC L      AUC I      GUC V
+UUA L      CUA L      AUA I      GUA V
+UUG L      CUG L      AUG M      GUG V
+UCU S      CCU P      ACU T      GCU A
+UCC S      CCC P      ACC T      GCC A
+UCA S      CCA P      ACA T      GCA A
+UCG S      CCG P      ACG T      GCG A
+UAU Y      CAU H      AAU N      GAU D
+UAC Y      CAC H      AAC N      GAC D
+UAA Stop   CAA Q      AAA K      GAA E
+UAG Stop   CAG Q      AAG K      GAG E
+UGU C      CGU R      AGU S      GGU G
+UGC C      CGC R      AGC S      GGC G
+UGA Stop   CGA R      AGA R      GGA G
+UGG W      CGG R      AGG R      GGG G"""
+
+coded = "AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA"
+decoded = ''
+
+traL =  string.split()
+traDict = dict(zip(traL[0::2], traL[1::2]))
+
+for i in range(0, len(coded)-3, 3):
+    decoded += traDict[coded[i:i+3]]
+
+print(decoded)

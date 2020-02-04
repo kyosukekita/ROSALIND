0ed5a18f058758099d5fc6a0abb7b4f381ebd70d
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2020-02-04 09:01:36 +0900

    Create longest_increasing_subsequence.py

---
+++ Bioinformatics Stronghold/longest_increasing_subsequence.py
@@ -0,0 +1,17 @@
+permutation="""5 1 4 2 3""" 
+
+permutation=permutation.split()
+permutation_list=[int(i) for i in permutation]
+
+#最長増加部分列
+import bisect
+def longest_increasing_subsequence(seq):
+    LIS=[seq[0]]
+    for i in seq[1:]:
+        if i >LIS[-1]:
+            LIS.append(i)
+        else:
+            LIS[bisect.bisect_left(LIS,i)]=i   
+    return LIS
+    
+print(*longest_increasing_subsequence(permutation_list))

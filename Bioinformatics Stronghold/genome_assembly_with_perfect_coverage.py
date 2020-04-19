31e7ae9b5ab394eb9044af22277f0571a48886d1
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2020-04-19 22:19:03 +0900

    Create genome_assembly_with_perfect_coverage.py

---
+++ Bioinformatics Stronghold/genome_assembly_with_perfect_coverage.py
@@ -0,0 +1,25 @@
+#http://rosalind.info/problems/pcov/solutions/
+
+strings= open('Desktop/Downloads/rosalind_pcov.txt').read()
+strings=[i for i in strings.split()]
+
+k=len(strings[0])-1#k-mer
+d={}
+
+#de Bruijin graph
+for string in strings:
+    d[string[:k]]=string[1:]
+#cycle from graph
+R=[]
+root=strings[0][:k]
+child=root
+while True:
+    R.append(child)
+    child=d[child]
+    if child ==root:
+        break
+
+genome=""
+for r in R:
+    genome +=r[0]
+print(genome)

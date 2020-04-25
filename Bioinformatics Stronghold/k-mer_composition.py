d130e02df465dab4c08e79e83d9e7d032ac05067
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2020-04-25 23:59:17 +0900

    Update k-mer_composition.py

--- Bioinformatics Stronghold/k-mer_composition.py
+++ Bioinformatics Stronghold/k-mer_composition.py
@@ -18,3 +18,16 @@ for i in range(len(dna)-4+1):
     
 for i in table.values():
     print(i, end= ' ') #end=' 'で改行無し
+
+    
+ 
+"別解"
+import collections
+import itertools as it
+
+def kmerComposition(dna, k):
+    return collections.Counter([dna[i:i+k] for i in range(len(dna)-k+1)]) #辞書型
+
+kmers = kmerComposition(dna, 4)
+for kmer in [''.join(s) for s in it.product('ACGT', repeat=4)]:
+    print(kmers[kmer], end=" ")

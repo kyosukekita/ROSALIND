9090ce3748819ffc04eb3c5dc62a2b6137f1c1ee
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2020-05-05 17:21:54 +0900

    Update memo.py

--- Bioinformatics Stronghold/memo.py
+++ Bioinformatics Stronghold/memo.py
@@ -16,4 +16,10 @@ for i in range(len(toy)):
     for nt in toy[i]:
         sess +=nt
         print(sess)
+        
+        
+        
+def catalan(n):
+    Cn=(math.factorial(2*n)/(math.factorial(n)**2)/(n+1))#Cn=2nCn/(n+1)
+    return int(Cn)
 

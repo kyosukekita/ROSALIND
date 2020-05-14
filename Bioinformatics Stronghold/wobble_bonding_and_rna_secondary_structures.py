3935ea8757bb548eba167425146b3cdcf1f7f0d4
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2020-05-15 00:30:47 +0900

    Create wobble_bonding_and_rna_secondary_structures.py

---
+++ Bioinformatics Stronghold/wobble_bonding_and_rna_secondary_structures.py
@@ -0,0 +1,22 @@
+s="""AUAUGUGAUCUGUAUGACUUACCGAUAGUAGCCCAUGGGGCACUAGCUUUGGUUUACCCUU
+AUUAGUACGUACUGCGGUCCUUCUAUCCUAGGCUCUGAUCCGAUCAGUAGCCGAAGGCAGCGAG
+AAACACGCCCGGCGUGUGUCUUCGAUAACCUAUGGUGUGUGACGUCAUCAUC"""
+
+cache={}
+def wobble(rna):
+    rna=rna.replace("\n","")
+    
+    if len(rna)==0 or len(rna)==1:
+        return 1
+    
+    if rna in cache:
+        return cache[rna]
+    
+    cache[rna]=wobble(rna[1:])
+    for i in range(4,len(rna)):
+        if (rna[0]=="A" and rna[i]=="U") or (rna[0]=="G" and rna[i]=="C") or(rna[0]=="G" and rna[i]=="U") or (rna[0]=="U" and rna[i]=="A") or (rna[0]=="U" and rna[i]=="G") or (rna[0]=="C" and rna[i]=="G"):
+            cache[rna]+=(wobble(rna[1:i])*wobble(rna[i+1:]))
+        
+    return cache[rna]
+
+wobble(s)

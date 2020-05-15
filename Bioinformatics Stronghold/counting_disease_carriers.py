dc4c861bb8fb21d7ddf30f7f4542156f92ff541b
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2020-05-15 22:10:03 +0900

    Update counting_disease_carriers.py

--- Bioinformatics Stronghold/counting_disease_carriers.py
+++ Bioinformatics Stronghold/counting_disease_carriers.py
@@ -6,7 +6,7 @@ B=[]
 for k in range(len(A)):
     q=math.sqrt(A[k])
     p=1-q
-    Bk=1-(p**2)
+    Bk=1-(p**2) #Hardy-Weinberg Principle states that (p^2) + (2pq) + (q^2) = 1
     B.append(Bk)
     
 print(*B)

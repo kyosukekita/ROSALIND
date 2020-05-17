c3857c641caf71e8ab881babc273ba35c10b196e
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2020-05-17 17:01:50 +0900

    Update speeding_up_motif_finding.py

--- Bioinformatics Stronghold/speeding_up_motif_finding.py
+++ Bioinformatics Stronghold/speeding_up_motif_finding.py
@@ -13,3 +13,37 @@ def kmp(string):
     return failure_array
 
 kmp(string)
+
+
+
+#KMP法の詳細
+#https://engineeringnote.hateblo.jp/entry/python/algorithm-and-data-structures/kmp_algorithm
+
+def create_table(pattern):
+    table = [0]*len(pattern)
+    j = 0
+    for i in range(1, len(pattern)):
+        if pattern[i] == pattern[j]:
+            j += 1
+            table[i] = j
+        else:
+            table[i] = j
+            j = 0
+    return table
+
+def kmp_search(string, pattern):
+    table = create_table(pattern)
+    i = j = 0
+    while i < len(string) and j < len(pattern):
+        if string[i] == pattern[j]:
+            i += 1
+            j += 1
+        elif j == 0:
+            i += 1
+        else:
+            j = table[j]
+ 
+    if j == len(pattern):
+        return i - j
+    else:
+        return None

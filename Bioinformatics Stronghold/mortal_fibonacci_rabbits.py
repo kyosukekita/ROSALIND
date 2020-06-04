dbde4c8385ff18d45a8a911e703667d5f6ae4089
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2020-06-04 22:54:32 +0900

    Update mortal_fibonacci_rabbits.py

--- Bioinformatics Stronghold/mortal_fibonacci_rabbits.py
+++ Bioinformatics Stronghold/mortal_fibonacci_rabbits.py
@@ -7,7 +7,7 @@ m=16
 def mortalFibonacci(n,m):
     ages=[1]+[0]*(m-1) #k歳のウサギの数を数える。ウサギの歳はm通り。
     for i in range(n-1):
-        ages=[sum(ages[1:])]+ages[:-1]
+        ages=[sum(ages[1:])]+ages[:-1]#子供たち+その親たち
     return sum(ages)
 
 mortalFibonacci(n,m)

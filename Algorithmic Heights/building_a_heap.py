a9656f89e81972cfa06a1237565d258621dbc1c4
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2020-06-06 09:50:30 +0900

    Create building_a_heap.py

---
+++ Algorithmic Heights/building_a_heap.py
@@ -0,0 +1,23 @@
+file = open('Desktop/Downloads/rosalind_hea.txt').read()
+n=int(file.split()[0])
+A=[int(i) for i in file.split()[1:n+1]]
+
+
+def swap(arr,i,j):
+    arr[i],arr[j]=arr[j],arr[i]
+    return arr
+
+
+def upheap(arr,i):
+    for i in range(1,n):
+        j=i
+        while j >0:
+            if arr[j]>arr[(j-1)//2]:
+                swap(arr,j,(j-1)//2)
+                j=(j-1)//2
+            else:
+                break
+    
+    print(' '.join(list(map(str,arr))))
+
+upheap(A,n)

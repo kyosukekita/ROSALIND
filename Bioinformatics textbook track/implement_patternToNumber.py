2fd73c7b5b6b024bf6e6aefcec1f2262416ddfa0
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2020-06-07 22:44:52 +0900

    Update implement_patternToNumber.py

--- Bioinformatics textbook track/implement_patternToNumber.py
+++ Bioinformatics textbook track/implement_patternToNumber.py
@@ -4,6 +4,6 @@ def patternToNumber(pattern):
     number="0b"#先頭に0bを付けることで二進数
     for i in range(len(pattern)):
         number += nucleotide[pattern[i]]
-    print(int(number,2))#2進数で表示
+    print(int(number,2))#2進数であることを認識させる。
 
 patternToNumber("AGT")

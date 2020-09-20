f5e1c2af2d34aa2446c1698c33c2124574d2e971
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2020-09-20 22:17:05 +0900

    Update majority_element.py

--- Algorithmic Heights/majority_element.py
+++ Algorithmic Heights/majority_element.py
@@ -1,6 +1,5 @@
 import collections
 
-
 file = open('Desktop/Downloads/rosalind_maj (1).txt').read()
 k,n=[int(i) for i in file.split("\n")[0].split()]
 A=[int(i) for i in file.split()[2:]]
@@ -20,3 +19,19 @@ for p in P:
         answer.append(-1)
 
 print(' '.join(map(str,answer)))
+
+
+
+
+
+#別解 Moore's Voting Algorithm
+def majority_element(a):
+    candidate,count=a[0],0 #初期化
+    #Run through the list, updating the count and changing candidates as necessary.
+    for element in a:
+        count += [-1,1][element == candidate]
+        if count ==0:
+            candidate, count =element,1
+    #Chech if the candidate is indeed the majority element
+    return [-1, candidate][a.count(candidate)> len(a)/2]
+    

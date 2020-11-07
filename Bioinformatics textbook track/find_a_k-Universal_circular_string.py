255a30692ce6ca041344b1f14f78f1b455852643
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2020-11-07 14:46:33 +0900

    Create find_a_k-Universal_circular_string.py

---
+++ Bioinformatics textbook track/find_a_k-Universal_circular_string.py
@@ -0,0 +1,44 @@
+from collections import defaultdict
+import collections   
+import itertools
+
+in_binary= 4
+lpos = in_binary-1
+
+last_binary ='1'*in_binary
+bin_int = int(last_binary, 2)
+
+last_before = "1"+('0'*lpos)
+first = '0'*in_binary
+nodes = defaultdict(list)
+for i in range(0,bin_int+1):
+    a = (bin(i)[2:].zfill(in_binary))
+    if (a!=last_before and a!=first):
+        s = a[0:lpos]
+        e = a[1:in_binary]
+        #print a, s,e
+        nodes[s].append(e)
+        nodes[e].append(s)
+print(nodes)       
+        
+tour = []
+start = '0'*(in_binary-1)
+tour.append(start)
+while(len(nodes[start])>0):
+    estart = start[1:len(start)]
+    if estart+"1" in  nodes[start]:
+        tour.append(estart+"1")
+        nodes[start].remove(estart+"1")
+        nodes[estart+"1"].remove(start)
+        start = estart+"1"
+    else:
+        tour.append(estart+"0")
+        nodes[start].remove(estart+"0")
+        nodes[estart+"0"].remove(start)
+        start = estart+"0"      
+
+ou_res ='0'
+for i,d in enumerate(tour):
+    ou_res+=d[0]
+
+print (ou_res)

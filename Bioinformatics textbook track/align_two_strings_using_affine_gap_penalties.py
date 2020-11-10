b23fd414b0cc6fbd6351b06a8c3d15e4b137c519
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2020-11-11 08:34:06 +0900

    Create align_two_strings_using_affine_gap_penalties.py

---
+++ Bioinformatics textbook track/align_two_strings_using_affine_gap_penalties.py
@@ -0,0 +1,81 @@
+s="""YVERCTYPRSIYSWFLKAMQEGSEQMKSHHTKQPPPTMRDWQQMDHAIGFGVYVAPIFDRRYLPGHGYTNAVQCTEGVDDSFMTFSM"""
+t="""YVERCTYPRSIYSWGSEQMKSHHTKFPPPTMRDSPQMDHLDPRMIGFGYPGHGYTNAVQCTFRM"""
+
+def global_align_with_affine(s,t):
+    s=s.replace('\n','')
+    t=t.replace('\n','')
+   
+    import pandas as pd
+    BLOSUM62=pd.DataFrame([
+    [4, 0, -2, -1, -2, 0, -2, -1, -1, -1, -1, -2, -1, -1, -1, 1, 0, 0, -3, -2],
+    [0, 9, -3, -4, -2, -3, -3, -1, -3, -1, -1, -3, -3, -3, -3, -1, -1, -1, -2, -2],
+    [-2, -3, 6, 2, -3, -1, -1, -3, -1, -4, -3, 1, -1, 0, -2, 0, -1, -3, -4, -3],
+    [-1, -4, 2, 5, -3, -2, 0, -3, 1, -3, -2, 0, -1, 2, 0, 0, -1, -2, -3, -2],
+    [-2, -2, -3, -3, 6, -3, -1, 0, -3, 0, 0, -3, -4, -3, -3, -2, -2, -1, 1, 3],
+    [0, -3, -1, -2, -3, 6, -2, -4, -2, -4, -3, 0, -2, -2, -2, 0, -2, -3, -2, -3],
+    [-2, -3, -1, 0, -1, -2, 8, -3, -1, -3, -2, 1, -2, 0, 0, -1, -2, -3, -2, 2],
+    [-1, -1, -3, -3, 0, -4, -3, 4, -3, 2, 1, -3, -3, -3, -3, -2, -1, 3, -3, -1],
+    [-1, -3, -1, 1, -3, -2, -1, -3, 5, -2, -1, 0, -1, 1, 2, 0, -1, -2, -3, -2],
+    [-1, -1, -4, -3, 0, -4, -3, 2, -2, 4, 2, -3, -3, -2, -2, -2, -1, 1, -2, -1],
+    [-1, -1, -3, -2, 0, -3, -2, 1, -1, 2, 5, -2, -2, 0, -1, -1, -1, 1, -1, -1],
+    [-2, -3, 1, 0, -3, 0, 1, -3, 0, -3, -2, 6, -2, 0, 0, 1, 0, -3, -4, -2],
+    [-1, -3, -1, -1, -4, -2, -2, -3, -1, -3, -2, -2, 7, -1, -2, -1, -1, -2, -4, -3],
+    [-1, -3, 0, 2, -3, -2, 0, -3, 1, -2, 0, 0, -1, 5, 1, 0, -1, -2, -2, -1],
+    [-1, -3, -2, 0, -3, -2, 0, -3, 2, -2, -1, 0, -2, 1, 5, -1, -1, -3, -3, -2],
+    [1, -1, 0, 0, -2, 0, -1, -2, 0, -2, -1, 1, -1, 0, -1, 4, 1, -2, -3, -2],
+    [0, -1, -1, -1, -2, -2, -2, -1, -1, -1, -1, 0, -1, -1, -1, 1, 5, 0, -2, -2],
+    [0, -1, -3, -2, -1, -3, -3, 3, -2, 1, 1, -3, -2, -2, -3, -2, 0, 4, -3, -1],
+    [-3, -2, -4, -3, 1, -2, -2, -3, -3, -2, -1, -4, -4, -2, -3, -3, -2, -3, 11, 2],
+    [-2, -2, -3, -2, 3, -3, 2, -1, -2, -1, -1, -2, -3, -1, -2, -2, -2, -1, 2, 7]],
+    index=['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y'],
+    columns=['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y'])
+ 
+    dp=[[0 for j in range(len(t) + 1)] for i in range(len(s) + 1)]
+    x=[[0 for j in range(len(t)+1)] for i in range(len(s)+1)] #s aligned to a gap
+    y=[[0 for j in range(len(t)+1)] for i in range(len(s)+1)] #t aligned to a gap
+    
+    for i in range(1,len(s) + 1):
+        dp[i][0] = -float('inf')
+        y[i][0]=(-1)*(i-1)-11
+    for i in range(1, len(t) + 1):
+        dp[0][i] = -float('inf')
+        x[0][i]= (-1)*(i-1)-11
+
+        
+    for i in range(1, len(s)+1):
+        for j in range(1,len(t)+1):
+            cost=BLOSUM62[s[i-1]][t[j-1]]            
+            x[i][j]=max(dp[i-1][j]-11, x[i-1][j]-1)
+            y[i][j]=max(dp[i][j-1]-11, y[i][j-1]-1)
+            
+            dp[i][j] = max(dp[i-1][j-1]+cost, x[i][j],y[i][j])
+                     
+    print(dp[len(s)][len(t)])
+    
+    s_align=''
+    t_align=''
+    i=len(s)
+    j=len(t)
+    
+    while (i>0 and j>0):
+        if dp[i][j] ==dp[i-1][j-1]+BLOSUM62[s[i-1]][t[j-1]]:
+            s_align = s[i-1]+s_align
+            t_align =t[j-1]+t_align
+            i -= 1
+            j -= 1
+        elif i>0 and dp[i][j]==x[i][j]:
+            s_align = s[i-1]+s_align
+            t_align ='-'+t_align
+            i -=1
+        elif j>0 and dp[i][j]==y[i][j]:
+            t_align =t[j-1]+t_align
+            s_align ='-' +s_align
+            j -= 1
+        else:
+            break
+
+    print(s_align)
+    print(t_align)
+
+
+global_align_with_affine(s,t)

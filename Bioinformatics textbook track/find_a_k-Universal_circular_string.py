ca92f06999f6e84b2bb83492c3788ff782a21f85
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2020-11-15 11:12:17 +0900

    Update find_a_k-Universal_circular_string.py

--- Bioinformatics textbook track/find_a_k-Universal_circular_string.py
+++ Bioinformatics textbook track/find_a_k-Universal_circular_string.py
@@ -1,3 +1,56 @@
+#find_a_k-universal_circular_string.py
+#reconstruct_a_string_from_its_kmer_composition.pyの続き,de-bruijin graphを使う、オイラー経路の問題
+k=4
+
+import itertools
+from collections import defaultdict
+
+kmers=[]#すべてのkmerをリストアップ  別解[bin(i)[2:].zfill(k) for i in range(2**k)]
+aa=itertools.product([0,1],repeat=k)
+for a in aa:
+    kmers.append("".join(map(str,a)))
+
+nodes=defaultdict(list)#de bruijin graph ここをdefaultdictにしたのは、キーが無くてもいけるから
+for kmer in kmers:
+    if kmer!="1"+"0"*(k-1) or kmer!="0"*k:
+        nodes[kmer[:-1]].append(kmer[1:])
+        nodes[kmer[1:]].append(kmer[:-1])#反対向きエッジも入れる
+
+
+#cycle from graph
+tour=[]
+start=kmers[0][:-1]#000
+tour.append(start)
+
+while(len(nodes[start])>0):
+    estart=start[1:len(start)]#00
+    if estart+"1" in nodes[start]:#001
+        tour.append(estart+"1")
+        nodes[start].remove(estart+"1")
+        nodes[estart+"1"].remove(start)
+        start=estart+"1"
+    else:
+        tour.append(estart+"0")#000
+        nodes[start].remove(estart+"0")
+        nodes[estart+"0"].remove(start)
+        start=estart+"0"
+
+answer="0"
+for _, d in enumerate(tour):
+    answer+=d[0]
+
+print(answer)
+
+
+
+
+
+
+
+
+
+
+#https://algorithmcode.wordpress.com/2014/11/18/string-reconstruction/
 from collections import defaultdict
 import collections   
 import itertools

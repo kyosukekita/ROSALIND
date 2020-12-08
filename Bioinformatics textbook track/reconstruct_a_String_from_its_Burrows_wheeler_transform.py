ec9741f1a1eca6008bdd66777222f0b2f8b751c3
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2020-12-08 21:23:50 +0900

    Create reconstruct_a_String_from_its_Burrows_wheeler_transform.py

---
+++ Bioinformatics textbook track/reconstruct_a_String_from_its_Burrows_wheeler_transform.py
@@ -0,0 +1,32 @@
+#http://smrmkt.hatenablog.jp/entry/2015/01/06/002300
+#https://naoya-2.hatenadiary.org/entry/20081016/1224173077
+
+file = open('Desktop/Downloads/rosalind_ba9j (1).txt').read()
+transform=(file.split("\n")[0])
+
+
+def inv_BWT(text):
+    #終端文字の位置を取得
+    p=text.index('$')
+    #各文字の出現回数をカウント
+    count={s:text.count(s) for s in set(list(text))}
+    #辞書順に並べたときの,各文字より前にある文字の合計個数を算出
+    #LFマッピングの右辺第2項を出すのに必要
+    
+    total=0
+    for k in sorted(count):
+        cur=count[k]
+        count[k] =total
+        total+=cur
+    
+    #LFのマッピングを行う
+    lf_mapping=[text[:i+1].count(text[i])+count[text[i]]-1 for i in range(len(text))]
+    #終端文字から逆順にたどる
+    ret=""
+    for i in range(len(text)-1):
+        ret=text[lf_mapping[p]]+ret
+        p=lf_mapping[p]
+    
+    return ret+"$"
+    
+print(inv_BWT(transform))

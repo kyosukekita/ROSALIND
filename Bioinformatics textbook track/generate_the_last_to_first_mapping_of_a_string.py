6a0ad2a7e2803fde6566dce29a0d4500c3b6795c
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2020-12-09 21:12:52 +0900

    Create generate_the_last_to_first_mapping_of_a_string.py

---
+++ Bioinformatics textbook track/generate_the_last_to_first_mapping_of_a_string.py
@@ -0,0 +1,22 @@
+#http://smrmkt.hatenablog.jp/entry/2015/01/06/002300
+#https://naoya-2.hatenadiary.org/entry/20081016/1224173077
+
+file = open('Desktop/Downloads/rosalind_ba9k.txt').read()
+transform=(file.split("\n")[0])
+i=int(file.split("\n")[1])
+
+
+def inv_BWT(text,i):
+    first = [x for x in range(len(text))]
+    first = sorted(first, key=lambda x: text[x])
+    output = ""
+    
+    idx = text.index('$')
+    for _ in range(len(text)):
+        before=idx
+        idx = first[idx]
+        if idx==i:
+            return before
+    
+    
+print(inv_BWT(transform,i))

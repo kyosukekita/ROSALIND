49a6e25fcadee19b3041f3f6fc165a51caadaf18
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2021-12-15 23:17:29 +0900

    Update construct_the_burrows-wheeler_transform_of_a_string.py

--- Bioinformatics textbook track/construct_the_burrows-wheeler_transform_of_a_string.py
+++ Bioinformatics textbook track/construct_the_burrows-wheeler_transform_of_a_string.py
@@ -6,7 +6,9 @@ text=(file.split("\n")[0])
 
 
 def BWT(text):
+    #ソート済みの接尾辞配列を作成
     sortedArray=sorted([text[i:] for i in range(len(text))])
+    #接尾辞が文字列全体と等しい場合は、文字列の最後の文字を使う
     return ''.join([text[-len(s)-1] if len(s)<len(text) else text[-1] for s in sortedArray])
     
 BWT(text)

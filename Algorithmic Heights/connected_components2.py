3ec999b213aa863d7c36f3698978ead5933588c6
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2022-11-06 14:36:57 +0900

    Update connected_components2.py

--- Algorithmic Heights/connected_components2.py
+++ Algorithmic Heights/connected_components2.py
@@ -1,4 +1,4 @@
-#幅優先探索を使って、連結成分の個数を調べる
+#幅（深さ）優先探索を使って、連結成分の個数を調べる
 #https://qiita.com/drken/items/996d80bcae64649a6580
 #https://algo-logic.info/connected-components-num/
 
@@ -9,7 +9,6 @@ edges=[tmp[i:i+2] for i in range(0,len(tmp),2)]
 #頂点数と辺数
 n,k= int(file.split()[0]), int(file.split()[1])
 
-
 seen=[False]*(n+1)
 def bfs(graph,start):
     seen[start]=True
@@ -17,7 +16,6 @@ def bfs(graph,start):
         if (seen[i]==False):
             bfs(graph,i)
             
-
 def Graph(edges):
     graph=[[] for _ in range(k)]
     for edge in edges:
@@ -26,8 +24,6 @@ def Graph(edges):
     
     return graph
     
-            
-
 def cc(graph):
     count=0
     for i in range(1,n):

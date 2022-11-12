730d365e3589d4081c271c44c242bb256b3fde52
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2022-11-12 13:17:06 +0900

    Update longest_increasing_subsequence.py

--- Bioinformatics Stronghold/longest_increasing_subsequence.py
+++ Bioinformatics Stronghold/longest_increasing_subsequence.py
@@ -17,34 +17,6 @@ def longest_increasing_subsequence(seq):
 #LISそのものは最長増加部分列になっているわけではないので注意。
 
 
-#正解
-class FindLongest:
-    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        memo = [[float('-inf') for j in range(len(text2)+1)]for i in range(len(text1)+1)]
-        for i in range(len(memo)):
-            for j in range(len(memo[0])):
-                if i == 0 or j == 0:
-                    memo[i][j] = 0
-                elif text1[i-1] == text2[j-1]:
-                    memo[i][j] = 1 + memo[i-1][j-1]
-                else:
-                    memo[i][j] = max(memo[i-1][j], memo[i][j-1])
-        i = len(memo)-1
-        j = len(memo[0])-1
-        ans = ""
-        while(i >= 0 and j >= 0):
-            if text1[i-1] == text2[j-1]:
-                ans += text1[i-1]
-                i -= 1
-                j -= 1
-            else:
-                if memo[i-1][j] > memo[i][j-1]:
-                    i -= 1
-                else:
-                    j -= 1
-        return(ans[::-1])
-
-
 #別解
 def lgis(n, π):
     S = [[]]*(n+1)

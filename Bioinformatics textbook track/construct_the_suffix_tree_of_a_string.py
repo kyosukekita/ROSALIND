c44d51384313d8afe4f3d2ce6355eeabac1a7f7c
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2022-11-12 15:29:15 +0900

    Update construct_the_suffix_tree_of_a_string.py

--- Bioinformatics textbook track/construct_the_suffix_tree_of_a_string.py
+++ Bioinformatics textbook track/construct_the_suffix_tree_of_a_string.py
@@ -6,8 +6,8 @@
 
 class Node:
     def __init__(self, sub="", children=None):
-        self.sub=sub#全てのエッジは空ではないsubstringによってラベル付けされる
-        self.ch=children or []#子ノードの番号
+        self.sub=sub #全てのエッジは空ではないsubstringによってラベル付けされる
+        self.ch=children or [] #子ノードの番号
 
 
 class SuffixTree:
@@ -21,38 +21,38 @@ class SuffixTree:
         n=0#今見ているノードの番号
         i=0#今見ているsuffixの文字の場所
         while i<len(suf):
-            b=suf[i]#suffixの中で、注目している文字をbとする
-            x2=0
+            b=suf[i]# suffixの中で、注目している文字をbとする
+            x=0
             while True:
-                children=self.nodes[n].ch#今注目しているノードの子供
-                if x2==len(children):#今注目しているノードの子供の個数とｘ2が等しければ、
+                children=self.nodes[n].ch #今注目しているノードの子供
+                if len(children)==x: #今注目しているノードの子供の個数とｘが等しければ、
                     #no matching child, remainder of suf becomes new node
-                    n2=len(self.nodes)#既に登録しているnodeの個数
-                    self.nodes.append(Node(suf[i:],[]))#今注目しているノードに、「suffixのi番目以降の文字」と「空の子供」を登録し、
-                    self.nodes[n].ch.append(n2)#ノードnの子供にn+1番の数字を登録する
+                    n1=len(self.nodes) #既に登録しているnodeの個数==n+1番目の数字
+                    self.nodes.append(Node(suf[i:],[])) #今注目しているノードに、「suffixのi番目以降の文字」と「空の子供」を登録し、
+                    self.nodes[n].ch.append(n1) #ノードnの子供にn+1番の数字を登録する
                     return
-                n2=children[x2]#今注目しているノードの子供のx2番目のノードの番号
-                if self.nodes[n2].sub[0]==b:
-                    #もしも「注目しているノードのsubstringの開始の文字」が、「suffixの中で、注目している文字」と等しければ、
-                    break#抜ける
-                x2 +=1#x2を一つ進める
+                n1=children[x] #今注目しているノードの子供のx番目のノードの番号
+                if self.nodes[n1].sub[0]==b:
+                    #もしも「注目しているノードのsubstringの開始の文字」が、「suffixの中で、注目している文字b」と等しければ、
+                    break #抜ける
+                x +=1 #xを一つ進める
             
             #find prefix of remaining suffix in common with child
-            sub2=self.nodes[n2].sub
+            sub1=self.nodes[n1].sub
             j=0
-            while j< len(sub2):
-                if suf[i+j]!=sub2[j]:
-                    #spilt n2
-                    n3=n2
+            while j< len(sub1):
+                if suf[i+j]!=sub1[j]:
+                    #spilt n1
+                    n2=n1
                     #new node for the past in common
-                    n2=len(self.nodes)#既に登録しているnodeの個数
-                    self.nodes.append(Node(sub2[:j],[n3]))
-                    self.nodes[n3].sub=sub2[j:] #old node loses the part in common
-                    self.nodes[n].ch[x2]=n2
+                    n1=len(self.nodes)#既に登録しているnodeの個数
+                    self.nodes.append(Node(sub2[:j],[n2]))
+                    self.nodes[n2].sub=sub2[j:] #old node loses the part in common
+                    self.nodes[n].ch[x1]=n1
                     break
                 j+=1
             i+=j
-            n=n2#continue down the tree
+            n=n1#continue down the tree
     
     def print(self):
         for i in range(1,len(self.nodes)):

932026fb5091a0ba2e4b20c60b6f2d79ba6700ab
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2023-11-26 15:03:55 +0900

    Update compute_the_probability_of_a_hidden_path.py

--- Bioinformatics textbook track/compute_the_probability_of_a_hidden_path.py
+++ Bioinformatics textbook track/compute_the_probability_of_a_hidden_path.py
@@ -17,3 +17,30 @@ for i in range(len(path)-1):
         probability=probability*BB
 
 print(probability)
+
+
+
+#compute the probability of a hidden path
+
+hidden_path="AABBBAABABAAAABBBBAABBABABBBAABBAAAABABAABBABABBAB"
+
+AA=0.194
+AB=0.806
+BA=0.273
+BB=0.727
+
+prob=0.5
+path=""
+for i in range(0, len(hidden_path)-1):
+    if hidden_path[i:i+2]=="AA":
+        prob*=AA
+    elif hidden_path[i:i+2]=="AB":
+        prob*=AB
+    elif hidden_path[i:i+2]=="BA":
+        prob*=BA
+    elif hidden_path[i:i+2]=="BB":
+        prob*=BB
+    path+=hidden_path[i]
+        
+print(prob)
+print(path)

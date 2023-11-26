7e6c3ed8a1610c7924987f7944c2e533e3375009
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2023-11-26 16:37:56 +0900

    Update compute_the_probability_of_a_string_emitted_by_an_HMM.py

--- Bioinformatics textbook track/compute_the_probability_of_a_string_emitted_by_an_HMM.py
+++ Bioinformatics textbook track/compute_the_probability_of_a_string_emitted_by_an_HMM.py
@@ -1,3 +1,41 @@
+file = open('/Users/kita/Downloads/rosalind_ba10d (1).txt').read()
+string=file.split("\n")[0]#output
+sigma=[i for i in file.split("\n")[2] if i!="	"] #入力するときは、スペースキーを除去してください。
+states=[i for i in file.split("\n")[4] if i!="	"]#state　#入力するときは、スペースキーを除去してください。
+transition=[]
+for i in range(7,7+len(states)):
+    transition.append([float(j) for j in file.split("\n")[i].split()[1:]])   
+emission=[]
+for i in range(9+len(states),9+len(states)+len(states)):
+    emission.append([float(j) for j in file.split("\n")[i].split()[1:]])
+    
+    
+#前回のviterbiアルゴリズムを少し改変
+prob=1/len(states)
+history=[{} for _ in range(len(string))]
+#history=[{"A":0.5, "B":0.5}, {"A":0.3, "B":0.4]] history[i]のAが0.3とは、pathがi番目の時にAになっている確率の最大が0.3
+backtrace=[{} for _ in range(len(string))]
+#backtrace=[{"A":"A", "B":"A"},{"A":"B","B":"A"}] backgrace[i]のAがAとは、pathがi番目の時にAになっている確率の最大がAの時、A→Aの遷移が起こったということ
+
+#初期設定
+for i in range(len(states)):
+    history[0][states[i]]=prob*emission[i][sigma.index(string[0])]# history=[{"A":0.5*Ax, "B":0.5*Bx}]
+
+for i in range(1,len(string)):
+    for j in range(len(states)):
+        register=0
+        for k in range(len(states)):
+            tra=transition[k][j]#history[i-1]["A"] or  history[i-1]["B"]→history[i]["A"] に遷移
+            emi=emission[j][sigma.index(string[i])]#A(i)→x(i)を出力           
+            
+            tmp = history[i-1][states[k]]*tra*emi
+            
+            register +=tmp#改変箇所←全てのtmpを合計した。
+        history[i][states[j]]=register
+
+sum(history[-1].values())
+
+
 #ビタビアルゴリズムのコードを少し改変
 file = open('Desktop/Downloads/rosalind_ba10d (2).txt').read()
 stringx=file.split("\n")[0]#output

97b4846fc051cdc0420be87db95e4ef281cbc42d
Kyosuke Kita <54225381+kyosukekita@users.noreply.github.com>
2024-01-04 23:38:03 +0900

    Update interleaving_two_motifs.py

--- Bioinformatics Stronghold/interleaving_two_motifs.py
+++ Bioinformatics Stronghold/interleaving_two_motifs.py
@@ -1,6 +1,7 @@
 #shortest common supersequence (SCSLength(X,Y)= m+n-LCSLength(X,Y))　典型的な動的計画法だが、この問題はLCSと密接に関係している。
 # https://python.plainenglish.io/print-shortest-common-supersequence-day-61-python-bba3ca1c288
 # https://devtut.github.io/algorithm/shortest-common-supersequence-problem.html#shortest-common-supersequence-problem-basic-information
+# https://dhwanishah2000.medium.com/shortest-common-super-sequence-in-dynamic-programming-5340479b2a57
 
 s="""TTACGGCCCCTACCCGTTAGACTGCTCTTCCGTGCTTAGCGGTGTCTCGAGTATTAGTACGGCCAGCACATTTGACCGTGTATTCCGGGCTA"""
 t="""AGGATAGTTTCTAAAAGCATACGAGCGATCCCTTGGATGAGAAGTCTCGACGTAGAGGAACTGCCGTTCTAGATGCCAACCAAGAGGATCGGAAAG"""
@@ -81,6 +82,70 @@ def SCS(x,y):
     return ''.join(string) 
 
 SCS(s,t)
+
+  
+
+
+# 参考 LCSとどこが違うか比較
+# https://medium.com/@finnluii/dp-1-longest-common-subsequence-5caf2c45f518
+# chatGPT4による回答
+# まず二つの文字列の最長共通部分列（LCS）を見つけ、その後、LCSを使ってSCSを構築します。
+def lcs(X, Y):
+    m = len(X)
+    n = len(Y)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+    for i in range(m + 1):
+        for j in range(n + 1):
+            if i == 0 or j == 0:
+                dp[i][j] = 0
+            elif X[i - 1] == Y[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+    return dp
+
+def scs(X, Y):
+    m, n = len(X), len(Y)
+    dp = lcs(X, Y)
+
+    # SCSを構築する
+    i, j = m, n
+    scs_str = ""
+
+    while i > 0 and j > 0:
+        # 現在の文字が両方の文字列に共通する場合
+        if X[i - 1] == Y[j - 1]:
+            scs_str += X[i - 1]
+            i -= 1
+            j -= 1
+        # そうでない場合、大きい方の値に従って移動する
+        elif dp[i - 1][j] > dp[i][j - 1]:
+            scs_str += X[i - 1]
+            i -= 1
+        else:
+            scs_str += Y[j - 1]
+            j -= 1
+
+    # 残りの文字を加える
+    while i > 0:
+        scs_str += X[i - 1]
+        i -= 1
+    while j > 0:
+        scs_str += Y[j - 1]
+        j -= 1
+
+    return scs_str[::-1]  # 逆順にして返す
+
+# 例
+X = "AGGTAB"
+Y = "GXTXAYB"
+print("Shortest Common Supersequence is", scs(X, Y))
+
+
+
+
 
 
 #参考
